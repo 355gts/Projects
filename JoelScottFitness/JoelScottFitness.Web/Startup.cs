338a19085d90e52b8b35eec2854223f5fdddbe5d
Simon <Blackmore__S@hotmail.com>
2017-04-24 21:29:54 +0100

    Initial commit

---
+++ JoelScottFitness/JoelScottFitness.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JoelScottFitness.Web.Startup))]
+namespace JoelScottFitness.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
