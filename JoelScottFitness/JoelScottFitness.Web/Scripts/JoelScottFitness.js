07f9c6696f030e0630750c06d40b2f2c00ec19ed
Simon <Blackmore__S@hotmail.com>
2017-05-21 23:17:41 +0100

    sorted image panels and nav menu

---
+++ JoelScottFitness/JoelScottFitness.Web/Scripts/JoelScottFitness.js
@@ -0,0 +1,22 @@
+﻿// nav menu scroll event handlers
+$(window).scroll(function () {
+    OffsetMenu();
+})
+
+$(window).resize(function () {
+    OffsetMenu();
+});
+
+// checks whether the nav menu should anchor to the top of the screen
+function OffsetMenu() {
+    var distance = $('.menu').offset().top;
+    var $window = $(window);
+
+    $('#nav').removeClass('sticky');
+    $('#nav-back-mask').hide();
+
+    if ($(window).scrollTop() >= $('.menu').offset().top) {
+        $('#nav').addClass('sticky');
+        $('#nav-back-mask').show();
+    }
+}
